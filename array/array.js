a336ba3384c4d19d1a1d1ec6ad1224395d3b1ce7
vitorkravs <vitorkravs3@gmail.com>
2022-09-20 15:59:34 -0300

    Create array.js

---
+++ array/array.js
@@ -0,0 +1,14 @@
+console.log(typeof Array, typeof new Array, typeof [])
+
+let aprovados =  new Array ('vitor', 'gabi', 'ana')
+console.log(aprovados)
+
+aprovados = ['Vitor', 'Gabi', 'Gustavo']
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[3])
+
+aprovados[3] = 'Kravs'
+aprovados.push('Lucidalia')
+console.log(aprovados.length)
