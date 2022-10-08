28bb22c810f226c0f4a3c13f898135517788bbd7
vitorkravs <vitorkravs3@gmail.com>
2022-10-08 00:02:20 -0300

    Update moduloClient.js

--- node/moduloClient.js
+++ node/moduloClient.js
@@ -2,9 +2,9 @@ const modulaA = require('./moduloA')
 const modulaB = require('./moduloB')
 
 console.log(modulaA.ola)
-console.log(modulaA.bemVindo)
-console.log(modulaA.ateLogo)
-console.log(modulaA)
+//console.log(modulaA.bemVindo)
+//console.log(modulaA.ateLogo)
+//console.log(modulaA)
 
 console.log(modulaB.bomDia)
 console.log(modulaB.boaNoite())
